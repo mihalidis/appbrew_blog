0d6a1f9d7179ff10362034f1d0c70c45de790da3
Pınar <pinarsuvac@gmail.com>
2021-01-17 21:02:05 +0300

    Database connect add

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require("lodash");
+require('dotenv').config()
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,36 +19,53 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Variables
-let posts = [];
+// Database Connection
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.on("error", console.error.bind(console, "connection error:"));
+connection.once("open",()=>{
+  console.log("MongoDB database connection established successfully");
+});
+
+//schema
+const PostSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+//model
+const Compose = mongoose.model("Compose", PostSchema);
+
+// Bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/",(req,res)=>{
-res.render("home",{startingContent:homeStartingContent, posts:posts});
+
+  Compose.find({},function(err, posts){
+    res.render(
+      "home",
+      {startingContent:homeStartingContent, 
+        posts:posts});
+  })
+
+  
 });
 
-app.get("/posts/:postName",(req,res)=>{
+app.get("/posts/:postId",(req,res)=>{
 
-  const reqTitle = _.lowerCase(req.params.postName);
+  const reqPostId = req.params.postId;
 
-  posts.forEach(post=>{
-
-    const linkTitle = _.lowerCase(post.title);
-
-    if(reqTitle === linkTitle){
-      
-      res.render("post",{
-        postTitle:post.title, 
-        postContent:post.content
-      });
-
-    }
-    else{
-      res.render("post",{postTitle:"Error", postContent:"Page Not Found!"});
-    }
-
+  Compose.findOne({_id:reqPostId}, (err,post)=>{
+    res.render("post",{
+      postTitle: post.title,
+      postContent: post.content
+    });
+  });
+ 
   });
 
-});
 
 app.get("/about",(req,res)=>{
   res.render("about",{aboutContent:aboutContent});
@@ -62,12 +82,15 @@ app.get("/compose",(req,res)=>{
 
 app.post("/compose",(req,res)=>{
   
-  const post = {
-    title:req.body.titleInput,
-    content:req.body.contentInput
-  };
-  posts.push(post);
-  res.redirect("/");
+  const post = new Compose({
+    title: req.body.titleInput,
+    content: req.body.contentInput
+  });
+  post.save(err => {
+    if(!err){
+      res.redirect("/");
+    }
+  });  
 });
 
 
